a3060b6810639941aa22b7fd2a11542aced816aa
tvrg <tvrg@a3a4976b-19da-0310-9c77-f36d2cd75c96>
2004-05-16 15:45:22 +0000

    even though it's not working, still updated it with my info

--- trunk/AboutDialog.cs
+++ trunk/AboutDialog.cs
@@ -9,11 +9,12 @@ public class AboutDialog
 
         public AboutDialog() 
         {
-        	string[] authors = {"Xavier Amado", ""};
+        	Console.Write("running about");
+        	string[] authors = {"Tom Vergote", ""};
 			Gdk.Pixbuf pixbuf = new Gdk.Pixbuf ("MonoIcon.png");
             
-			Gnome.About aboutdlg = new Gnome.About ("MonoBrowser", "0.1", "Copyright 2003 Xavier Amado", 
-			"A Mono powered web browser.", authors, null, null, pixbuf);
+			Gnome.About aboutdlg = new Gnome.About ("gtkRanker", "0.0", "Copyright 2004 Tom Vergote", 
+			"Tool to check your ranking in Google", authors, null, null, pixbuf);
 			
 			aboutdlg.Run();
 		}
