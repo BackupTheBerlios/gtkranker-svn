ce9553144404b48356e5d721ab755ee89c3512b8
tvrg <tvrg@a3a4976b-19da-0310-9c77-f36d2cd75c96>
2004-05-18 12:11:33 +0000

    Check for configuration directory at startup

--- trunk/Main.cs
+++ trunk/Main.cs
@@ -8,6 +8,14 @@ public class gtkRanker
 {
 	public static void Main (string[] args)
 	{
+		// check if we have a configuration directory
+		string configdir = System.Environment.GetEnvironmentVariable("HOME");
+		configdir = System.IO.Path.Combine(configdir, ".gtkranker");
+		
+		if (!System.IO.Directory.Exists(configdir))
+		{
+			System.IO.Directory.CreateDirectory(configdir);
+		}
 		new GUI.Mainwindow ();
 	}
 }
