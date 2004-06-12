88d19b91ccc0c9e5949130483b611994772833bf
tvrg <tvrg@a3a4976b-19da-0310-9c77-f36d2cd75c96>
2004-06-12 16:05:40 +0000

    started moving to sqlite for the backend

--- trunk/src/lib/libWebsites.cs
+++ trunk/src/lib/libWebsites.cs
@@ -3,15 +3,16 @@ using System;
 using System.IO;
 using System.Xml;
 using System.Text;
+using System.Data;
 using System.Collections.Specialized;
+using Mono.Data.SqliteClient;
 
 namespace ranker.lib
 {
 	public class libWebsites
 	{
-	
-		// Xmldocument for the websites xml file
-		XmlDocument doc;
+		IDbConnection dbcon;
+		IDbCommand dbcmd;
 		
 		public libWebsites()
 		{
@@ -21,62 +22,45 @@ namespace ranker.lib
 	
 		private void LoadConfiguration()
         {
-        	doc = new XmlDocument();	
-			
-			string xmlpath = System.Environment.GetEnvironmentVariable("HOME");
-			xmlpath = Path.Combine(xmlpath, ".gtkranker/websites.xml");
-			
-			// Check if the file exists
-			if(!File.Exists(xmlpath))
-			{
-				// Create an empty root element
-				doc.AppendChild(doc.CreateNode(XmlNodeType.Element, "sitelist", ""));
-				// Save the file
-				this.SaveConfiguration();
-			}
-			else
-			{
-				doc.Load(xmlpath);
-			}
+			string connectionString = "URI=file:"+System.Environment.GetEnvironmentVariable("HOME")+ Path.DirectorySeparatorChar + ".gtkranker/websites.db";
+			dbcon = new SqliteConnection(connectionString);
+			dbcon.Open();
+			dbcmd = dbcon.CreateCommand();        	
         }        
         
         
 		void SaveConfiguration()
 		{
-			string xmlpath = System.Environment.GetEnvironmentVariable("HOME");
-			xmlpath = Path.Combine(xmlpath, ".gtkranker/websites.xml");
-			doc.Save(xmlpath);	
+				
 		}
 		
 		public void FillStoreNames(Gtk.TreeStore tree_store)
 		{
 			// Populate the model.
-			XmlNodeList nodes = doc.SelectNodes("//site");
-			foreach (XmlNode node in nodes)
+			dbcmd.CommandText = "select name from websites";
+			IDataReader reader = dbcmd.ExecuteReader();
+			while (reader.Read())
 			{
-				string name = node.Attributes["name"].InnerText;
+				string name = reader.GetString(0);
 				tree_store.AppendValues(name);
 			}
 		}
 		
 		public string GetSiteUrl(string name)
 		{
-			XmlNode node = doc.SelectSingleNode("//site[@name='" + name + "']");
-			string url = node.Attributes["url"].InnerText;
+			dbcmd.CommandText = "select url from websites where name = '" + name.Replace("'","''") + "'";
+			string url = (string)dbcmd.ExecuteScalar();
 			return url;
 		}
 		
 		public StringCollection GetSiteKeywords(string name)
 		{
-			// Populate the model.
-			string xpath = "/sitelist/site[@name='" + name + "']/keyphrase";
-			XmlNodeList nodes = doc.SelectNodes(xpath);
-			int i=0;
 			StringCollection keywords=new StringCollection();
-			foreach (XmlNode node in nodes)
+			dbcmd.CommandText = "select keyphrase from keywords where sitename = '" + name.Replace("'","''") + "'";
+			IDataReader reader = dbcmd.ExecuteReader();
+			while (reader.Read())
 			{
-				string kw = node.InnerText;
-				keywords.Add(kw);
+				keywords.Add(reader.GetString(0));				
 			}
 			return keywords;
 		}
@@ -84,37 +68,37 @@ namespace ranker.lib
 		public void addItem(string name, string url, string keywords)
 		{
 			//select root node
-			XmlNode foldernode = doc.SelectSingleNode("/sitelist");
-			//create our new site's element
-			XmlNode newitem =doc.CreateNode(XmlNodeType.Element, "site", "");
+//			XmlNode foldernode = doc.SelectSingleNode("/sitelist");
+//			//create our new site's element
+//			XmlNode newitem =doc.CreateNode(XmlNodeType.Element, "site", "");
 			//Add the name and url attributes
-			string ns = newitem.GetNamespaceOfPrefix("bk");
-			XmlNode attr = doc.CreateNode(XmlNodeType.Attribute, "name",ns);
-			attr.Value = name;
-			newitem.Attributes.SetNamedItem(attr);
-			attr = doc.CreateNode(XmlNodeType.Attribute, "url",ns);
-			attr.Value = url;
-			newitem.Attributes.SetNamedItem(attr);
-			//Add the keywords childs
-			string [] aKeywords = keywords.Split(";"[0]);
+//			string ns = newitem.GetNamespaceOfPrefix("bk");
+//			XmlNode attr = doc.CreateNode(XmlNodeType.Attribute, "name",ns);
+//			attr.Value = name;
+//			newitem.Attributes.SetNamedItem(attr);
+//			attr = doc.CreateNode(XmlNodeType.Attribute, "url",ns);
+//			attr.Value = url;
+//			newitem.Attributes.SetNamedItem(attr);
+//			//Add the keywords childs
+//			string [] aKeywords = keywords.Split(";"[0]);
 
-        	foreach (string s in aKeywords) 
-        	{
-				XmlNode keywordnode =doc.CreateNode(XmlNodeType.Element, "keyphrase", "");
-				keywordnode.InnerText = s;
-				newitem.AppendChild(keywordnode);    
-        	}
-			foldernode.AppendChild(newitem);
-			this.SaveConfiguration();
-			Console.WriteLine("Added new website");
+//        	foreach (string s in aKeywords) 
+ //       	{
+//				XmlNode keywordnode =doc.CreateNode(XmlNodeType.Element, "keyphrase", "");
+//				keywordnode.InnerText = s;
+//				newitem.AppendChild(keywordnode);    
+ //       	}
+//			foldernode.AppendChild(newitem);
+//			this.SaveConfiguration();
+//			Console.WriteLine("Added new website");
 		}
 		
 		public void deleteItem(string name)
 		{
-			XmlNode root = doc.DocumentElement;
-			XmlNode node = root.SelectSingleNode("//site[@name='" + name + "']");
-			root.RemoveChild(node);
-			this.SaveConfiguration();
+//			XmlNode root = doc.DocumentElement;
+//			XmlNode node = root.SelectSingleNode("//site[@name='" + name + "']");
+//			root.RemoveChild(node);
+//			this.SaveConfiguration();
 		}
 	}
 }
